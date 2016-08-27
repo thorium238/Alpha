6e754baa37548cdfb929670a23887b291d604e8e
Thomas Piotrowski <Thomas.Piotrowski@gmail.com>
2016-08-27 12:05:28 -0500

    before renaming viewController

---
+++ Alpha/CreateTaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateTaskViewController.swift
+//  Alpha
+//
+//  Created by Thomas Piotrowski on 8/26/16.
+//  Copyright © 2016 Thomas Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    //var previousViewController =
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+       
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: AnyObject) {
+        // Create a Task from the outlets info.
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        // Add new task to array in previous viewcontroller
+    }
+
+}
